2b2a797229ace07df5806cf2353b99ab1496ccf6
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-20 12:05:49 +0700

    Update XinChao.java

--- HelloWorld/src/pl/XinChao.java
+++ HelloWorld/src/pl/XinChao.java
@@ -13,7 +13,7 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TÔI LẦN ĐẦU HỌC Java, và đang thử nghiệm quẩn lý mã nguồn GIT");
+		System.out.println("TÔI LẦN ĐẦU HỌC Java, và đang thử nghiệm quản lý mã nguồn GIT");
 		System.out.print("Tôi thêm một dòng mới, và sửa lại chữ Java ở trên");
 	}
 
