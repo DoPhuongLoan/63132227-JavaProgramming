387202fe8227b4e87d932648b4552df025e7c01c
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-20 11:39:14 +0700

    bài mẫu hello

---
+++ HelloWorld/src/pl/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pl;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TÔI LẦN ĐẦU HỌC JAVA, và đang thử nghiệm quẩn lý mã nguồn GIT");
+
+	}
+
+}
