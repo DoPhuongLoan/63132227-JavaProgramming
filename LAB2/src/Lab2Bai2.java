369f649350dbdc0d140e067a76a4b0037e19d999
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-23 00:37:02 +0700

    Bài 2 Lab2

--- LAB2/src/Lab2Bai2.java
+++ LAB2/src/Lab2Bai2.java
@@ -10,28 +10,29 @@ public class Lab2Bai2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		double a, b, c ;
-        System.out.print("Nhập hệ số a: ");
-        a = scanner.nextDouble();
-        System.out.print("Nhập hệ số b: ");
-        b = scanner.nextDouble();
-        System.out.print("Nhập hệ số c: ");
+		double a, b, c;
+		System.out.print("Nhập hệ số a: ");
+		a = scanner.nextDouble();
+		System.out.print("Nhập hệ số b: ");
+		b = scanner.nextDouble();
+		System.out.print("Nhập hệ số c: ");
 		c = scanner.nextDouble();
-    	double x = -c/b;
-    	if(a == 0) {
-			if(b == 0) {
-				if(c == 0) {
+		double x = -c/b;
+		if(a==0){
+			if(b==0){
+				if(c==0){
 					System.out.print("Phương trình vô số nghiệm");
 				}
-				else {
+				else{
 					System.out.print("Phương trình vô nghiệm");
 				}
 			}
 			else {
-				System.out.printf("Nghiem cua phuong trinh la: %f", x);
+				System.out.printf("Phương trình có nghiệm duy nhất: %f", x);
 			}
-    	}
-        
+		}
+		else {
+			
+		}
 	}
-
 }
