76fc69addcb7075e8ffbbe03e3a0063eacd6df9d
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-22 22:36:34 +0700

    Bài 3 Lab1

--- LAB1/src/Lab1Bai3.java
+++ LAB1/src/Lab1Bai3.java
@@ -10,9 +10,10 @@ public class Lab1Bai3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		double canh, thetich;
 		System.out.print("Nhập cạnh khối lập phương: ");
-		double canh = scanner.nextDouble();
-		double thetich = canh*canh*canh;
+		canh = scanner.nextDouble();
+		thetich = canh*canh*canh;
 		System.out.printf("Thể tích khối lập phương: %.1f", thetich);
 		
 	}
