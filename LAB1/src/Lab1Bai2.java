c98a3a2dae746ea0c70edc9f497dced0bd4fd674
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-22 21:54:08 +0700

    Bài 2 Lab1

---
+++ LAB1/src/Lab1Bai2.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+import java.util.Scanner;
+public class Lab1Bai2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		double cd, cr;
+		System.out.print("Nhập chiều dài: ");
+		cd = scanner.nextDouble();
+		System.out.print("Nhập chiều rộng: ");
+		cr = scanner.nextDouble();
+		
+		
+	}
+
+}
