ab2bcdb78161aa4e05da926eea7e8c371883d4a3
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-24 01:22:57 +0700

    Demo
    Kiểm tra số dương và tính căn bậc 2

--- Demo/src/KiemTraSo1.java
+++ Demo/src/KiemTraSo1.java
@@ -6,16 +6,18 @@
  * 
  */
 import java.util.Scanner;
-public class KiemTraSo {
+public class KiemTraSo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Nhập số: ");
 		double so = scanner.nextDouble();
-		if (so > 0) {
-			double canso = Math.sqrt(so);
-			System.out.println("Căn bậc 2 của " + so + " : " + canso);
-		}
+		if(so > 0) {
+			double luythua = Math.sqrt(so);
+			System.out.println("Căn bậc 2 " + so + " : " + luythua);
+		}else {
+			System.out.println("Số nhập vào không phải là số dương");
+        }
 	}
 
 }
