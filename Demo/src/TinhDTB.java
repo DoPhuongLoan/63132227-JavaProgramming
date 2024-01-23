c691e1a22fca0d1aff96c8a80d64640480edbcbe
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-23 23:49:13 +0700

    Demo
    Tính điểm trung bình

---
+++ Demo/src/TinhDTB.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class TinhDTB {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 8.5;
+		double b = 7.0;
+		double c = 6.8;
+		int hsa = 2;
+		double dtb = (a * hsa + b + c)/3;
+		
+		System.out.println("Điểm trung bình: " + dtb);
+	}
+
+}
