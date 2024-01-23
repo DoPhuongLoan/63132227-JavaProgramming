3e32297a5a605f0510224e861d01b1e4419794f2
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-23 22:57:15 +0700

    Demo
    Cộng Hai Số Nguyên

---
+++ Demo/src/CongHaiSoNguyen.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+
+public class CongHaiSoNguyen {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 15, b = 5, c;
+		c = a + b;
+		System.out.println("a + b = " + c);
+
+	}
+
+}
