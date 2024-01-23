280e86cf1a1b9c61a4ddf8bd53bc44b2b9d1a859
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-24 00:09:15 +0700

    Demo
    Thông tin

---
+++ Demo/src/ThongTin.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class ThongTin {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hoten = "Đỗ Phương Loan";
+		int tuoi = 21;
+		
+		System.out.println(hoten + " năm nay " + tuoi + " tuổi");
+	}
+
+}
