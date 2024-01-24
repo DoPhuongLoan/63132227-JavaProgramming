7d3516c0cf7502d5b7b025c46c4e0e132cd3f2e0
DoPhuongLoan <phuongloan2003nt@gmail.com>
2024-01-25 00:05:57 +0700

    Bài 2 Lab3

---
+++ LAB3/src/Lab3Bai2.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Lab3Bai2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("Bảng cửu chương %d:\n", i);
+			for (int j = 1; j <= 10; j++) {
+				System.out.printf("%d x %d = %d\n", i, j, i * j);
+			}
+		}
+	}
+}
